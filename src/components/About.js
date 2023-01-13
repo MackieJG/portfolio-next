3ffa469ff8c9b0eb64c09ad8a31b8f6844cdacbf
MackieJG <joshuamackie10@googlemail.com>
2023-01-13 15:44:27 +0000

    updating homepage and about page

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+    <div>
+        <h4>About</h4>
+            <p>I'm currently learning to be a Professional Software Developer with 
+                <a href="https://codeclan.com/courses/professional-software-development/">CodeClan.</a>
+                So far I've developed a solo Inventory Management App using python, flask, postgresSQL, css and html. 
+                We're currently working towards developing our JavaScript knowledge with the project approaching in the next few weeks 
+                giving us the opportunity to showcase our React knowledge. 
+            </p>
+    </div>
+    )
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+    <div>
+        <h4>Hello! I'm Joshua Mackie and welcome to my portfolio page</h4>
+    </div>
+    )
+};
+
+export default Home;
